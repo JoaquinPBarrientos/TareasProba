64e0a58f2416557e27dd78561d0259ad913c51cf
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-10-17 00:42:18 -0300

    Agregando archivos

---
+++ T1/TareaProba.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import itertools as it
+import scipy as sp
+import matplotlib.pyplot as plt
+import scipy.stats as st # ocupar para distribuciones
+
+
+
+# Abrimos la BD.
+data_base = pd.read_csv() 
+
