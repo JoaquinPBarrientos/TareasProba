e5fbe6926cd49e30351e482b7caf592f699d524a
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-10-17 14:40:35 -0300

    P2 Lista

--- T1/TareaProba.py
+++ T1/TareaProba.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import scipy.stats as st # ocupar para distribuciones
 import numpy as np
 import math
+import seaborn as sns
 # Pregunta 1
 
 
@@ -13,12 +14,14 @@ import math
 
 data_base = pd.read_csv('data.csv')
 data_base = pd.DataFrame(data_base)
-print(data_base)
+print("La base de datos es la siguiente:\n")
+print(data_base,"\n")
 
 	# P2.A
 	# Aqui generamos nuestra tablas de frecuencias.
 
 tabla_frecuencias = pd.crosstab( data_base["Numeros"],columns ="Frecuencias")
+print("La tabla de frecuencias es la siguiente:\n ")
 print(tabla_frecuencias)
 
 	# P2.B
@@ -33,18 +36,40 @@ plt.show()
 print(tabla_frecuencias)
 	# P2.C
 	# Hay que calcular el promedio con la funcion .mean()
+	# Grafiquemos
 
+d = st.norm(promedio,des_standard)
+
+v = d.rvs(50)
+plt.hist(d.rvs(1000),normed = True)
+plt.show()
 promedio = (data_base.mean())[0]
-print(promedio)
+print("El promedio obtenido es",promedio)
 
 	# Ahora obtendremos la desviacion estandar.
 des_standard = (data_base.std())[0]
-print(des_standard)
+print("La desviación estándar es",des_standard)
+	# Con ambos datos pordemos obtener el Coeficiente de Variacion
+
+	# Al ojo, es una desviación bastante "Normal" si la comparamos con la media y los datos.
+
+
+
+	#P2.D
+CV = des_standard/promedio
+print("El coeficiente de variación es",CV)
+CV_porcentual = CV*100
+print("El coeficiente de variación porcentual es",CV_porcentual)
+	#Como podemos ver el porcentaje es de 6.9% lo que está mucho más abajo de 80% , por lo que el promedio 
+	# es representativo del conjunto de datos, por lo tanto el conjunto de datos es homogeneo.
+
+	# RESPUESTA: Solo un 6.9% 
 
 # Pregunta 3
 #data_base = pd.DataFrame(data_base)
 
 #print(data_base)
+
 
 
 
