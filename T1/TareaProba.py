37d9df6f57cb96870378a8c8e158b4d425700579
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-10-17 13:10:06 -0300

    Avance de pregunta 2

--- T1/TareaProba.py
+++ T1/TareaProba.py
@@ -12,10 +12,21 @@ import scipy.stats as st # ocupar para distribuciones
 # Abrimos la BD.
 data_base = pd.read_csv('data.csv')
 data_base = pd.DataFrame(data_base)
-
 print(data_base)
 
+	# P1.A
+	# Aqui generamos nuestra tablas de frecuencias.
+tabla_frecuencias = pd.crosstab( data_base["Numeros"],columns ="Frecuencias")
+print(tabla_frecuencias)
+	#P1.B
+	# Como son frecuencias absolutas, lo mejor es ver la cantidad de de veces que se repite cada valor.
+	#Entonces utilizaremos diagrama de barras.
+plt.bar(tabla_frecuencias.index, tabla_frecuencias["Frecuencias"])
+plt.xlabel("Minutos usados")
+plt.ylabel("Frecuencia")
+plt.show(True)
 
+print(tabla_frecuencias)
 
 
 
