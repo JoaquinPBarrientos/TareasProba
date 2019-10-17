00b947e8a4f28a7cee86f0cd66fa8eff8a468c84
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-10-17 12:20:18 -0300

    Arreglada base de datos para pregunta 2.

--- T1/TareaProba.py
+++ T1/TareaProba.py
@@ -5,9 +5,24 @@ import matplotlib.pyplot as plt
 import scipy.stats as st # ocupar para distribuciones
 
 
+# Pregunta 1
 
+
+# Pregunta 2
 # Abrimos la BD.
-data_base = pd.read_csv('data.xlsx')
+data_base = pd.read_csv('data.csv')
 data_base = pd.DataFrame(data_base)
 
 print(data_base)
+
+
+
+
+
+# Pregunta 3
+#data_base = pd.DataFrame(data_base)
+
+#print(data_base)
+
+
+
