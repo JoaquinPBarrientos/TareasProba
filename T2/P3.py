0404ca6d24b338d3f3f26c9106b456f752518e4c
Joaquin Barrientos <joaquinbarrientos@MacBook-Pro-de-Joaquin-4.local>
2019-11-18 22:47:45 -0300

    P3 lista

--- T2/P3.py
+++ T2/P3.py
@@ -5,11 +5,16 @@ import numpy as np
 import math
 
 
+
+
 muestra = np.array([35000, 44000, 38000, 55000, 33000, 56000, 67000, 45000])
 media = np.mean(muestra) #Media muestral
-sigma = np.std(muestra) #Desviacion estandar muestral
+sigma = np.std(muestra)  #Desviacion estandar muestral
 n = len(muestra) #largo de la muestra
 gl = n - 1
+
+print(sigma)
+
 #Utilizamos t de student debido a que es una muestra pequeña y conocemos la varianza muestral
 #P3.A
 alpha = 0.02 #alpha 
@@ -18,18 +23,19 @@ n_confianza = 1 - alpha
 print(st.t.interval(n_confianza, gl, loc = media, scale = sigma/math.sqrt(n)))
 
 #P3.B
-#NO SE RECHAZA 
+#
 alpha = 0.05
 μ0 = 47000
-# En este caso queremos comprobar que μ > 47 
-# si t_obt > t1-aplha entonces se rechaza H0: μ>μ0
+# En este caso queremos comprobar que H0: μ > 47 
+# Nuestra solución será hacer el caso H1: μ < 47
+# si t_obt < t_alpha entonces se rechaza H0
 
 def Test_Hipostesis_P3_B(n,alpha,μ0,media,sigma):
 
 	t_obt = (media-μ0)/(sigma/math.sqrt(n))
 	gl = n - 1
-	t_alpha = st.t.sf(alpha,gl)
-
+	t_alpha = st.t.ppf(alpha,gl)
+	print(t_alpha)
 
 	
 	if 	t_obt > t_alpha:
@@ -43,3 +49,16 @@ def Test_Hipostesis_P3_B(n,alpha,μ0,media,sigma):
 
 
 Test_Hipostesis_P3_B(n,alpha,μ0,media,sigma)
+
+#P3.C 
+
+
+def Intervalo_Confianza(alpha,desviacion_muestral,gl):
+
+	upper = (gl*(sigma**2))/st.chi2.ppf(0.05,gl)
+	lower = (gl*(sigma**2))/st.chi2.ppf(1-alpha/2,gl)
+	print("(",lower,",",upper,")")
+
+Intervalo_Confianza(alpha,sigma,gl)
+
+
